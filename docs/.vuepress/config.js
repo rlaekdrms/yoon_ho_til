be03d27ae92ade74aceea5d29c28134cab9ca208
rlaekdrms <danielyoonho@naver.com>
2025-01-14 20:14:38 +0900

    base update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,7 @@ export default defineUserConfig({
 
   title: "윤호의 TIL",
   description: "윤호 블로그",
+  base: "yoon_ho_til",
 
   theme: defaultTheme({
     logo: "https://mblogthumb-phinf.pstatic.net/20151202_260/jju423_1449058382856rckgl_PNG/qqjzZi7ajs.png?type=w800",
