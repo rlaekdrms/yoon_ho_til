14f1b08f493f2ad10f61d47aad5186cd6cf328c1
rlaekdrms <danielyoonho@naver.com>
2025-01-14 20:04:24 +0900

    2025-01-14 업데이트트

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,15 @@ export default defineUserConfig({
   theme: defaultTheme({
     logo: "https://mblogthumb-phinf.pstatic.net/20151202_260/jju423_1449058382856rckgl_PNG/qqjzZi7ajs.png?type=w800",
 
-    navbar: ["/", "/get-started"],
+    navbar: ["/", "/til/"],
+    sidebar: {
+      "/til/": [
+        {
+          text: "TIL프로젝트",
+          children: ["README.md", "2025-01-09.md", "2025-01-14.md"],
+        },
+      ],
+    },
   }),
 
   bundler: viteBundler(),
---
+++ docs/til/README.md
@@ -0,0 +1,16 @@
+# TIL 프로젝트
+
+TIL 프로젝트 소개 문서에요.
+
+## 파이썬 코드
+
+```python
+from fastapi import FastAPI
+
+app = FastAPI()
+```
+
+## 강에서 엄마 오리를 잃어버린 노란 오리
+
+![노란오리](../.vuepress/public/큰오리.jpeg)
+<img :src="$withBase('큰오리.jpeg')" alt="무료로 계정 만들기">
